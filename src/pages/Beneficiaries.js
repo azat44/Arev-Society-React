12800b50c15e5cd73ec84eb3931d89e54ed7b0ca
azat44 <azat4beats@gmail.com>
2024-07-16 16:13:57 +0200

    beneficiaries page done

--- src/pages/Beneficiaries.js
+++ src/pages/Beneficiaries.js
@@ -141,12 +141,122 @@ class Beneficiaries extends React.Component {
                                 <a href="women-empowerment-story-6">I need to stay strong</a>
                             </h3>
                             <div className="article-story-6">
-                                0
+                                
                                 When news first arrived that Nvard's husband had gone missing during
                                 the war, a deep sadness fell upon their household...
                                 <a href="women-empowerment-story-6">Read the story</a>
                             </div>
+                            
                         </div>
+                        <div className="story-7">
+                            <img
+                                className="img-story-7"
+                                src={require('../images/Arev-Society-Women-Empowerment-Story-07.webp')}
+                                alt="Women Empowerment in Armenia - 07 - Arev Society "
+                            />
+                            <p className="date-story-7">Women Empowerment</p>
+                            <h3 className="title-story-7">
+                                <a href="women-empowerment-story-7">I am the author of my story</a>
+                            </h3>
+                            <div className="article-story-7">
+                                
+                            Anna’s peaceful life was paradise until the war took her husband away. 
+                            Now, it was up to her to decide whether to give up and condemn her family...
+                                <a href="women-empowerment-story-7">Read the story</a>
+                            </div>
+                            
+                        </div>
+                        <div className="story-8">
+                            <img
+                                className="img-story-8"
+                                src={require('../images/Arev-Society-Women-Empowerment-Story-08.webp')}
+                                alt="Women Empowerment in Armenia - 08 - Arev Society"
+                            />
+                            <p className="date-story-8">Women Empowerment</p>
+                            <h3 className="title-story-8">
+                                <a href="women-empowerment-story-8">I do not know the expression I cannot</a>
+                            </h3>
+                            <div className="article-story-8">
+                                
+                            The 2020 war in Artsakh has left Astghik’s, among many other vulnerable women’s, 
+                            peaceful lives wholly ruptured. During those obscure and disheartening times...
+                                <a href="women-empowerment-story-8">Read the story</a>
+                            </div>
+                            
+                        </div>
+                        <div className="story-9">
+                            <img
+                                className="img-story-9"
+                                src={require('../images/Arev-Society-Women-Empowerment-Story-09.webp')}
+                                alt="Women Empowerment in Armenia - 09 - Arev Society"
+                            />
+                            <p className="date-story-9">Women Empowerment</p>
+                            <h3 className="title-story-9">
+                                <a href="women-empowerment-story-9">I couldn't sit by idly and had to do something myself</a>
+                            </h3>
+                            <div className="article-story-9">
+                                
+                            Hasmik welcomed us to her mother's house in Sevan, Armenia, where she had arranged her atelier studio...
+                                <a href="women-empowerment-story-9">Read the story</a>
+                            </div>
+                            
+                        </div>
+                        <div className="story-10">
+                            <img
+                                className="img-story-10"
+                                src={require('../images/Arev-Society-Women-Empowerment-Story-10.webp')}
+                                alt="Women Empowerment in Armenia - 10 - Arev Society"
+                            />
+                            <p className="date-story-10">Women Empowerment</p>
+                            <h3 className="title-story-10">
+                                <a href="women-empowerment-story-10">Zhanna’s Life Story</a>
+                            </h3>
+                            <div className="article-story-10">
+                                
+                            I was the happiest woman in the world when my husband was still by my side. 
+                            His mere presence was enough for me to wake up each day with gratitude...
+                                <a href="women-empowerment-story-10">Read the story</a>
+                            </div>
+                            
+                        </div>
+                        <div className="story-11">
+                            <img
+                                className="img-story-11"
+                                src={require('../images/Arev-Society-Women-Empowerment-Story-11.webp')}
+                                alt="Women Empowerment in Armenia - 11 - Arev Society"
+                            />
+                            <p className="date-story-11">Women Empowerment</p>
+                            <h3 className="title-story-11">
+                                <a href="women-empowerment-story-11">I had to become someone who would pull us together</a>
+                            </h3>
+                            <div className="article-story-11">
+                                
+                            used to be fearless and proud. When my family and I fled from Artsakh to Armenia, 
+                            I had lost all hope of a stable, happy life...
+                                <a href="women-empowerment-story-11">Read the story</a>
+                            </div>
+                            
+                        </div>
+                        <div className="story-12">
+                            <img
+                                className="img-story-12"
+                                src={require('../images/Arev-Society-Women-Empowerment-Story-12.webp')}
+                                alt="Women Empowerment in Armenia - 12 - Arev Society"
+                            />
+                            <p className="date-story-12">Women Empowerment</p>
+                            <h3 className="title-story-12">
+                                <a href="women-empowerment-story-12">They share our joy and sorrow- From Artsakh to Shgarshik</a>
+                            </h3>
+                            <div className="article-story-12">
+                                
+                            We arrived directly from Artsakh (Nagorno-Karabakh) in late September 2023 without anything 
+                            because we didn't have enough time to prepare...
+                                <a href="women-empowerment-story-12">Read the story</a>
+                            </div>
+                            
+                            
+                        </div>
+                        
                     </div>
                 </main>
             </>
